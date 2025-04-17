4b9a392ae3b72ba2bf1f2d2468d05192e0988442
lavor <lavor.andrey@gmail.com>
2025-04-17 09:03:26 +0600

    Rename extensions to customizers

--- intellispaces-ixora-testcases-http-ontology/src/main/java/tech/intellispaces/ixora/testcases/http/simple/testcase1/SimpleHttpPortAssistantCustomizer.java
+++ intellispaces-ixora-testcases-http-ontology/src/main/java/tech/intellispaces/ixora/testcases/http/simple/testcase1/SimpleHttpPortAssistantCustomizer.java
@@ -1,16 +1,13 @@
 package tech.intellispaces.ixora.testcases.http.simple.testcase1;
 
 import tech.intellispaces.ixora.http.MovableInboundHttpPort;
-import tech.intellispaces.jaquarius.annotation.Factory;
+import tech.intellispaces.jaquarius.annotation.AssistantCustomizer;
 import tech.intellispaces.jaquarius.object.reference.DownwardObjectFactory;
 
-@Factory
-public class SimpleHttpPortFactory implements SimpleHttpPortAssistantCustomizer {
+@AssistantCustomizer(SimpleHttpPortDomain.class)
+public interface SimpleHttpPortAssistantCustomizer {
 
-  @Override
-  public MovableSimpleHttpPortHandle create(
+  MovableSimpleHttpPortHandle create(
       DownwardObjectFactory<? extends MovableInboundHttpPort> underlyingPortHandleFactory
-  ) {
-    return new SimpleHttpPortImplWrapper(underlyingPortHandleFactory);
-  }
+  );
 }
